3f53f8d38fe951d9e1bfb31e656310d49a59f872
Jittipha <93649348+Jittipha@users.noreply.github.com>
2023-05-16 15:34:20 +0700

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
  async function db() {
     
-    mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qf5hft4.mongodb.net/?retryWrites=true&w=majority`).then(()=>console.log('connected')).catch(err => console.log('Error :'+err))
+    mongoose.connect(`mongodb+srv://Nanine:II4fQOigYIr6ub25@cluster0.qf5hft4.mongodb.net/?retryWrites=true&w=majority`).then(()=>console.log('connected')).catch(err => console.log('Error :'+err))
 }
 
 module.exports = db
